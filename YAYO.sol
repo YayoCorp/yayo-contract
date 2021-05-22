53b65ecad27bc144f6b5e209f5539cb78e089f81
Yayo <yayocoin@protonmail.com>
2021-05-23 00:14:08 +0200

    add Viral Public Licence to contract

--- YAYO.sol
+++ YAYO.sol
@@ -31,6 +31,23 @@
 
 // SPDX-License-Identifier: Viral Public License
 
+/*
+
+VIRAL PUBLIC LICENSE
+Copyleft (ɔ) All Rights Reversed
+
+This WORK is hereby relinquished of all associated ownership, attribution and copy
+rights, and redistribution or use of any kind, with or without modification, is
+permitted without restriction subject to the following conditions:
+
+1.  Redistributions of this WORK, or ANY work that makes use of ANY of the
+    contents of this WORK by ANY kind of copying, dependency, linkage, or ANY
+    other possible form of DERIVATION or COMBINATION, must retain the ENTIRETY
+    of this license.
+2.  No further restrictions of ANY kind may be applied.
+
+*/
+
 pragma solidity ^0.8.4;
 
 interface IERC20 {
